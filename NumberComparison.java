dadc3ce758ce17455f1f50c7755a27626e236cfa
Prakanshu703 <152187043+Prakanshu703@users.noreply.github.com>
2024-07-24 17:55:55 +0530

    Add files via upload

---
+++ NumberComparison.java
@@ -0,0 +1,23 @@
+package NDemo;
+
+public class NumberComparison
+{
+    public static void main(String[] args)
+    {
+        int numerator = 10;
+        int denominator = 20;
+
+        if (numerator > denominator) 
+        {
+            System.out.println("Greater");
+        } 
+        else if (numerator == denominator)
+        {
+            System.out.println("Equal");
+        } 
+        else 
+        {
+            System.out.println("Less");
+        }
+    }
+}
